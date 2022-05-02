689d15a936a100d2bf83e3b52a1ed38524dc6e57
Jeffrey Kozlowski <jeffrey.kozlowski@me.com>
2022-05-02 00:32:38 -0500

    Add numPoints spinner, write pointcurve and bspline method sigs

--- src/coffee/views/CurveMakerView.java
+++ src/coffee/views/CurveMakerView.java
@@ -9,6 +9,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -16,9 +17,10 @@ import javafx.scene.layout.HBox;
 public class CurveMakerView extends BorderPane {
 
 	private GridPane grid;
-	private HBox hbox;
 	private static final String OPTION_1 = "Point-Driven Spline";
 	private static final String OPTION_2 = "B-Spline Curve";
+	private static final int MIN_CONTROL_POINTS = 2;
+	private static final int MAX_CONTROL_POINTS = 4;
 
 	public CurveMakerView() {
 		super();
@@ -26,15 +28,21 @@ public class CurveMakerView extends BorderPane {
 	}
 
 	private void init() {
-		grid = new GridPane();
-		hbox = new HBox();
+		initGrid();
 		makeSelectorBox();
+		makeNumPointsSpinner();
+	}
+
+	private void initGrid() {
+		grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setPadding(new Insets(20, 0, 20, 0));
+		grid.setVgap(10);
+		grid.setHgap(10);
+		setTop(grid);
 	}
 
 	private void makeSelectorBox() {
-		HBox selectorBox = new HBox();
-		selectorBox.setAlignment(Pos.CENTER);
-		selectorBox.setPadding(new Insets(20, 0, 20, 0));
 		ObservableList<String> optionsList = FXCollections.observableArrayList(OPTION_1, OPTION_2);
 		ComboBox<String> comboBox = new ComboBox<String>(optionsList);
 		comboBox.setPromptText("Curve Style");
@@ -46,17 +54,33 @@ public class CurveMakerView extends BorderPane {
 				selectCurveStyle(((ComboBoxBase<String>) event.getSource()).getValue());
 			}
 		});
-		selectorBox.getChildren().add(comboBox);
-		setTop(selectorBox);
+		Label label = new Label("Curve Style:");
+		grid.add(label, 0, 0);
+		grid.add(comboBox, 1, 0);
 	}
 
 	private void selectCurveStyle(String curveStyle) {
 		if (curveStyle == null)
 			return;
-		//TODO: modify behavior here
+		// TODO: modify behavior here
 		if (curveStyle.equals(OPTION_1))
-			setCenter(new Label(OPTION_1));
+			setCenter(pointCurvePane());
 		if (curveStyle.equals(OPTION_2))
-			setCenter(new Label(OPTION_2));
+			setCenter(bSplinePane());
+	}
+
+	private void makeNumPointsSpinner() {
+		Spinner<Integer> spinner = new Spinner<Integer>(MIN_CONTROL_POINTS, MAX_CONTROL_POINTS, MIN_CONTROL_POINTS);
+		Label label = new Label("# of Control Points:");
+		grid.add(label, 0, 1);
+		grid.add(spinner, 1, 1);
+	}
+	
+	private GridPane pointCurvePane() {
+		return null;
+	}
+	
+	private GridPane bSplinePane() {
+		return null;
 	}
 }
